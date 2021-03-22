1e325260cc33cb548ea291fb86640d5eb61826ea
michael beamer <beamermichael@gmail.com>
2021-03-22 16:27:41 +0100

    added fixed navBar

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// Sticky navigation: Intersection Observer API
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight - 35}px`,
+});
+
+headerObserver.observe(header);
+
+// Smooth scrolling
+
+document.querySelector(".main-nav").addEventListener("click", function (event) {
+  event.preventDefault(); //matching strategy
+  if (event.target.classList.contains("nav-item")) {
+    const id = event.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
