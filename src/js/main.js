19f7330769fe995acdd03fc6c9e9171af9aeab42
michael beamer <beamermichael@gmail.com>
2021-03-22 20:47:55 +0100

    fixed

--- src/js/main.js
+++ src/js/main.js
@@ -26,31 +26,3 @@ document.querySelector(".main-nav").addEventListener("click", function (event) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
-
-// Animation: Fade in
-const phone = document.querySelector(".app-display");
-
-function revealSection(entries, observer) {
-  const [entry] = entries;
-  console.log(entry);
-
-  if (!entry.isIntersecting) return;
-  entry.target.classList.add("fadeInUp");
-  entry.target.classList.remove("hidden");
-  observer.unobserve(entry.target);
-}
-
-const revealOptions = {
-  root: null,
-  threshold: 0.8,
-};
-
-const phoneObserver = new IntersectionObserver(revealSection, revealOptions);
-phoneObserver.observe(phone);
-
-// let waypoint = new Waypoint({
-//   element: document.getElementById("waypoint"),
-//   handler: function (direction) {
-//     console.log("Scrolled to waypoint!");
-//   },
-// });
