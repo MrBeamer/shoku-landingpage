5f5905cff71719b58e09f130c517333403f1adb3
michael beamer <beamermichael@gmail.com>
2021-03-22 20:45:44 +0100

    delete]

--- src/js/main.js
+++ src/js/main.js
@@ -28,25 +28,25 @@ document.querySelector(".main-nav").addEventListener("click", function (event) {
 });
 
 // Animation: Fade in
-const phone = document.querySelector(".app-display");
+// const phone = document.querySelector(".app-display");
 
-function revealSection(entries, observer) {
-  const [entry] = entries;
-  console.log(entry);
+// function revealSection(entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry);
 
-  if (!entry.isIntersecting) return;
-  entry.target.classList.add("fadeInUp");
-  entry.target.classList.remove("hidden");
-  observer.unobserve(entry.target);
-}
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.add("fadeInUp");
+//   entry.target.classList.remove("hidden");
+//   observer.unobserve(entry.target);
+// }
 
-const revealOptions = {
-  root: null,
-  threshold: 0.8,
-};
+// const revealOptions = {
+//   root: null,
+//   threshold: 0.8,
+// };
 
-const phoneObserver = new IntersectionObserver(revealSection, revealOptions);
-phoneObserver.observe(phone);
+// const phoneObserver = new IntersectionObserver(revealSection, revealOptions);
+// phoneObserver.observe(phone);
 
 // let waypoint = new Waypoint({
 //   element: document.getElementById("waypoint"),
